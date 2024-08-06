e00abba5a19e1f06d020901ec4a76d886762e5c2
Dan <danielcrump@outlook.com>
2024-08-06 15:02:46 -0400

    make height min

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="h-screen">
+    <Html >
       <Head />
-      <body className="h-full">
+      <body className="min-h-screen">
         <Main />
         <NextScript />
       </body>
