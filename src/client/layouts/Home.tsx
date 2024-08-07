89c18783f250e5f19e6f75dbd63a234bf1aefc1d
Dan <danielcrump@outlook.com>
2024-08-07 16:29:18 -0400

    add to home template wip

--- src/client/layouts/Home.tsx
+++ src/client/layouts/Home.tsx
@@ -3,7 +3,14 @@
 import React from "react";
 
 function Home() {
-  return <div className="dark:text-white h-full">The Home Page</div>;
+	return (
+		<div className="dark:text-white dark:bg-zinc-800">
+			{/* Hero component */}
+			<div className="h-screen text-center"> hero component </div>
+			{/* Content area */}
+			<div className="h-[60vh] text-center">content</div>
+		</div>
+	);
 }
 
 export default Home;
