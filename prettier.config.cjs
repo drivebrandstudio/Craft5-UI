35ff4d80b412772b799f2824a71da2c7fea33adc
Dan <danielcrump@outlook.com>
2024-08-07 16:25:29 -0400

    update prettier config to match shadCN w/ changes

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,46 +1,30 @@
 module.exports = {
-  overrides: [
-    {
-      files: ["*.twig"],
-      options: {
-        "tabWidth": 3,
-        "useTabs": true,
-        "semi": false,
-        "singleQuote": true,
-        "twigPrintWidth": 120,
-
-        "twigMultiTags": [
-          "nav,endnav",
-          "switch,case,default,endswitch",
-          "ifchildren,endifchildren",
-          "cache,endcache",
-          "js,endjs"
-        ]
-      }
-    }
-  ],
-  plugins: ["./node_modules/prettier-plugin-twig-melody", "@ianvs/prettier-plugin-sort-imports"],
-  importOrder: [
-    "^(react/(.*)$)|^(react$)",
-    "^(next/(.*)$)|^(next$)",
-    "<THIRD_PARTY_MODULES>",
-    "",
-    "^types$",
-    "^@/types/(.*)$",
-    "^@/config/(.*)$",
-    "^@/lib/(.*)$",
-    "^@/hooks/(.*)$",
-    "^@/components/ui/(.*)$",
-    "^@/components/(.*)$",
-    "^@/styles/(.*)$",
-    "^@/app/(.*)$",
-    "",
-    "^[./]"
-  ],
- importOrderSeparation: false,
-  importOrderSortSpecifiers: true,
-  importOrderBuiltinModulesToTop: true,
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderMergeDuplicateImports: true,
-  importOrderCombineTypeAndValueImports: true
-}
+	endOfLine: "lf",
+	semi: true,
+	singleQuote: false,
+	trailingComma: "es5",
+	tabWidth: 3,
+	useTabs: true,
+	plugins: ["@ianvs/prettier-plugin-sort-imports"],
+	importOrder: [
+		"^(react/(.*)$)|^(react$)",
+		"",
+		"^(next/(.*)$)|^(next$)",
+		"",
+		"<THIRD_PARTY_MODULES>",
+		"",
+		"^types$",
+		"^@/types/(.*)$",
+		"^@/config/(.*)$",
+		"",
+		"^@/client/(.*)$",
+		"",
+		"^[./]",
+	],
+	importOrderSeparation: false,
+	importOrderSortSpecifiers: true,
+	importOrderBuiltinModulesToTop: true,
+	importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+	importOrderMergeDuplicateImports: true,
+	importOrderCombineTypeAndValueImports: true,
+};
