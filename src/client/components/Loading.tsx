f0643028f795ce9f023feba3cc0639c679147889
Dan <danielcrump@outlook.com>
2024-08-08 16:44:07 -0400

    remove container class from loading

--- src/client/components/Loading.tsx
+++ src/client/components/Loading.tsx
@@ -1,72 +1,73 @@
-'use client'
+"use client";
 
 import React from "react";
+
 import { cn } from "../utils/cn";
 
 // Not all loading screens should take up 100% height (EG. weather widget)
 const Loading = ({ fullScreen }: { fullScreen?: boolean }) => {
-  return (
-    <section
-      className={cn(
-        "flex justify-center items-center",
-        fullScreen && "h-screen "
-      )}
-    >
-      <div className="container">
-        <div className="ring"></div>
-        <div className="ball-context-layer ball-context-layer--1">
-          <div className="ball-container">
-            <div className="ball"></div>
-          </div>
-        </div>
-        <div className="ball-context-layer ball-context-layer--2">
-          <div className="ball-container">
-            <div className="ball"></div>
-          </div>
-        </div>
-        <div className="ball-context-layer ball-context-layer--3">
-          <div className="ball-container">
-            <div className="ball"></div>
-          </div>
-        </div>
-        <div className="ball-context-layer ball-context-layer--4">
-          <div className="ball-container">
-            <div className="ball"></div>
-          </div>
-        </div>
-        <div className="ball-context-layer ball-context-layer--5">
-          <div className="ball-container">
-            <div className="ball"></div>
-          </div>
-        </div>
-        <div className="ball-context-layer ball-context-layer--6">
-          <div className="ball-container">
-            <div className="ball"></div>
-          </div>
-        </div>
-        <div className="ball-context-layer ball-context-layer--7">
-          <div className="ball-container">
-            <div className="ball"></div>
-          </div>
-        </div>
-        <div className="ball-context-layer ball-context-layer--8">
-          <div className="ball-container">
-            <div className="ball"></div>
-          </div>
-        </div>
-        <div className="ball-context-layer ball-context-layer--9">
-          <div className="ball-container">
-            <div className="ball"></div>
-          </div>
-        </div>
-        <div className="ball-context-layer ball-context-layer--10">
-          <div className="ball-container">
-            <div className="ball"></div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section
+			className={cn(
+				"flex justify-center items-center",
+				fullScreen && "h-screen "
+			)}
+		>
+			<div className="loadingContainer">
+				<div className="ring"></div>
+				<div className="ball-context-layer ball-context-layer--1">
+					<div className="ball-container">
+						<div className="ball"></div>
+					</div>
+				</div>
+				<div className="ball-context-layer ball-context-layer--2">
+					<div className="ball-container">
+						<div className="ball"></div>
+					</div>
+				</div>
+				<div className="ball-context-layer ball-context-layer--3">
+					<div className="ball-container">
+						<div className="ball"></div>
+					</div>
+				</div>
+				<div className="ball-context-layer ball-context-layer--4">
+					<div className="ball-container">
+						<div className="ball"></div>
+					</div>
+				</div>
+				<div className="ball-context-layer ball-context-layer--5">
+					<div className="ball-container">
+						<div className="ball"></div>
+					</div>
+				</div>
+				<div className="ball-context-layer ball-context-layer--6">
+					<div className="ball-container">
+						<div className="ball"></div>
+					</div>
+				</div>
+				<div className="ball-context-layer ball-context-layer--7">
+					<div className="ball-container">
+						<div className="ball"></div>
+					</div>
+				</div>
+				<div className="ball-context-layer ball-context-layer--8">
+					<div className="ball-container">
+						<div className="ball"></div>
+					</div>
+				</div>
+				<div className="ball-context-layer ball-context-layer--9">
+					<div className="ball-container">
+						<div className="ball"></div>
+					</div>
+				</div>
+				<div className="ball-context-layer ball-context-layer--10">
+					<div className="ball-container">
+						<div className="ball"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Loading;
