0ed655512b695005f93324090a911f92f468cc90
dancrump1 <danielcrump@outlook.com>
2024-08-20 15:38:58 -0400

    removing sean starter assumptions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,46 @@
 import React from "react";
+
 import { GetStaticProps } from "next";
 import dynamic from "next/dynamic";
 
 import cmsClient from "@/server/cmsClient";
 import { HomeEntryQuery } from "@/server/gql/home.gql";
-import { pageQueries } from "@/server/gql/page.gql";
 import { NavQuery } from "@/server/gql/nav.gql";
+import { pageQueries } from "@/server/gql/page.gql";
 
 export interface IPageProps {
-  [k: string]: any;
+	[k: string]: any;
 }
 
 const App = dynamic(import("../client/layouts/Home"));
 
 function Index(props: IPageProps): JSX.Element {
-  return <App data={props.data} />;
+	return <App data={props.data} />;
 }
 
 export default Index;
 
 export const getStaticProps: GetStaticProps = async ({
-  params,
-  preview,
-  previewData,
+	params,
+	preview,
+	previewData,
 }) => {
-  const client = cmsClient(preview, previewData?.token);
-  const data = await client.request(HomeEntryQuery, {
-    uri: "__home__",
-  });
+	const client = cmsClient(preview, previewData?.token);
+	const data = await client.request(HomeEntryQuery, {
+		uri: "__home__",
+	});
 
-  const seoReq = await client.request(pageQueries.landingPages, {
-    uri: "__home__",
-  });
+	// const seoReq = await client.request(pageQueries.landingPages, {
+	//   uri: "__home__",
+	// });
 
-  const nav = await client.request(NavQuery);
+	// const nav = await client.request(NavQuery);
 
-  return {
-    props: {
-      seo: seoReq.entry?.seo || null,
-      routes: nav.navEntries,
-      data: data.homeEntry,
-    },
-  };
+	return {
+		props: {
+			// seo: seoReq.entry?.seo || null,
+			// routes: nav.navEntries,
+			data: data,
+		},
+	};
 };
--- src/server/gql/home.gql.ts
+++ src/server/gql/home.gql.ts
@@ -1,10 +1,7 @@
 import { gql } from 'graphql-request'
 
 export const HomeEntryQuery = gql`
-	query PagesPagesEntryQuery($uri: [String], $slug: [String]) { 
-    homeEntry: entry(uri: $uri, slug: $slug) {
-        id
-        uri  
-    }
+	{ 
+    ping
 	}
 `
