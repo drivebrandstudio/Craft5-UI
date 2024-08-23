58ec099828d89aaa5d0ca24bef64ba406aa39fc8
dancrump1 <danielcrump@outlook.com>
2024-08-23 15:27:37 -0400

    add next config images and timeline comp

--- next.config.js
+++ next.config.js
@@ -1,24 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export", // Outputs a Single-Page Application (SPA).
-  // distDir: "./dist", // Changes the build output directory to `./dist/`.
-  // basePath: '', // Sets the base path to `/some-base-path`.
-  // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-  eslint: {
-     ignoreDuringBuilds: true,
-  },
-   // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-  typescript: {
-    
-    ignoreBuildErrors: true,
-  }
-
+	// output: "export", // Outputs a Single-Page Application (SPA).
+	// distDir: "./dist", // Changes the build output directory to `./dist/`.
+	// basePath: '', // Sets the base path to `/some-base-path`.
+	// !! WARN !!
+	// Dangerously allow production builds to successfully complete even if
+	// your project has type errors.
+	// !! WARN !!
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	// !! WARN !!
+	// Dangerously allow production builds to successfully complete even if
+	// your project has type errors.
+	// !! WARN !!
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "[website_name].com",
+				port: "",
+				pathname: "/[filesystem_name]/**",
+			},
+		],
+	},
 };
- 
-export default nextConfig
+
+export default nextConfig;
