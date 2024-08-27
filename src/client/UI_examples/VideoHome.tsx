709a96b180b35de57345026e6a868923955f1d16
dancrump1 <danielcrump@outlook.com>
2024-08-27 11:35:25 -0400

    add floating dock

--- src/client/UI_examples/VideoHome.tsx
+++ src/client/UI_examples/VideoHome.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import FAQ from "../components/FAQ";
 import CtaContent from "../components/library/CtaContent";
 import GridContent from "../components/library/GridContent";
+import { LinkPreview } from "../components/library/LinkPreview";
 import { TextHoverEffect } from "../components/library/TextHover";
 import VideoHero from "../components/library/VideoHero";
 
-const VideoHome = () => {
+const VideoHome = ({ data }) => {
 	return (
 		<main className="dark:text-white dark:bg-zinc-800">
 			{/* Hero component */}
@@ -17,6 +18,31 @@ const VideoHome = () => {
 			<FAQ />
 			<div className="h-[40rem] flex items-center justify-center">
 				<TextHoverEffect text="DRIVE" />
+			</div>
+			<div className="flex justify-center items-center h-[40rem] flex-col px-4">
+				<p className="text-neutral-500 dark:text-neutral-400 text-xl md:text-3xl max-w-3xl  text-left mb-10">
+					Visit
+					<LinkPreview
+						url="https://google.com"
+						imageSrc={data.assets[0].url}
+						className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500"
+					>
+						Google
+					</LinkPreview>
+					and for amazing Tailwind and Framer Motion components.
+				</p>
+
+				<p className="text-neutral-500 dark:text-neutral-400 text-xl md:text-3xl max-w-3xl  text-left ">
+					I listen to{" "}
+					<LinkPreview
+						url="https://google.com"
+						imageSrc={data.assets[1].url}
+						className="font-bold"
+					>
+						this guy
+					</LinkPreview>
+					and I watch
+				</p>
 			</div>
 		</main>
 	);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,14 @@ import Seo from "@/client/components/Seo";
 
 import "../client/scss/main.scss";
 
+import Image from "next/image";
+
+import { AiOutlineProduct } from "react-icons/ai";
+import { CgComponents } from "react-icons/cg";
+import { FaExchangeAlt, FaInstagram, FaTwitter } from "react-icons/fa";
+import { SiGooglehome } from "react-icons/si";
+
+import { FloatingDock } from "@/client/components/library/FloatingDock";
 import ResizeNavBar from "@/client/components/ResizeNavbar";
 
 export default function AppRoot({
@@ -17,15 +25,79 @@ export default function AppRoot({
 }: AppProps): JSX.Element {
 	const { seo, routes, ...props } = pageProps;
 
+	const links = [
+		{
+			title: "Home",
+			icon: (
+				<SiGooglehome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+			),
+			href: "#",
+		},
+
+		{
+			title: "Products",
+			icon: (
+				<AiOutlineProduct className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+			),
+			href: "#",
+		},
+		{
+			title: "Components",
+			icon: (
+				<CgComponents className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+			),
+			href: "#",
+		},
+		{
+			title: "Aceternity UI",
+			icon: (
+				<Image
+					src="https://assets.aceternity.com/logo-dark.png"
+					width={20}
+					height={20}
+					alt="Aceternity Logo"
+				/>
+			),
+			href: "#",
+		},
+		{
+			title: "Changelog",
+			icon: (
+				<FaExchangeAlt className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+			),
+			href: "#",
+		},
+
+		{
+			title: "Twitter",
+			icon: (
+				<FaTwitter className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+			),
+			href: "#",
+		},
+		{
+			title: "Instagram",
+			icon: (
+				<FaInstagram className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+			),
+			href: "#",
+		},
+	];
+
 	return (
 		<>
 			<Seo {...seo} />
 			<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 				{/* NAVIGATION COMPONENTS start */}
 				{/* <NavBar routes={routes} /> */}
-				<NavBar2 routes={routes} />
+				<div className="hidden md:block">
+					<NavBar2 routes={routes} />
+				</div>
 				{/* <NavBar3 /> */}
 				{/* <ResizeNavBar /> */}
+				<div className="md:hidden fixed inset-x-0 bottom-[15px] flex items-center justify-center z-50">
+					<FloatingDock items={links} />
+				</div>
 				{/* NAVIGATION COMPONENTS end */}
 
 				{/* SRC > CLIENT > LAYOUTS start */}
