fee114ec9ebaea5d1805c67cb05347be9f605cb8
dancrump1 <danielcrump@outlook.com>
2024-08-27 09:41:24 -0400

    clean up backgrounds

--- src/client/components/library/CtaContent.tsx
+++ src/client/components/library/CtaContent.tsx
@@ -2,39 +2,37 @@ import React from "react";
 
 const CtaContent = () => {
 	return (
-		<section>
-			<div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 text-white">
-				<div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-					<div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-						<img
-							alt=""
-							src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-							className="absolute inset-0 h-full w-full object-cover"
-						/>
-					</div>
+		<div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 text-white">
+			<div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+				<div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+					<img
+						alt=""
+						src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+						className="absolute inset-0 h-full w-full object-cover"
+					/>
+				</div>
 
-					<div className="lg:py-24">
-						<h2 className="text-3xl font-bold sm:text-4xl text-black dark:text-white">
-							Grow your audience
-						</h2>
+				<div className="lg:py-24">
+					<h2 className="text-3xl font-bold sm:text-4xl text-black dark:text-white">
+						Grow your audience
+					</h2>
 
-						<p className="mt-4 text-gray-300 dark:text-white">
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-							Aut qui hic atque tenetur quis eius quos ea neque sunt,
-							accusantium soluta minus veniam tempora deserunt? Molestiae
-							eius quidem quam repellat.
-						</p>
+					<p className="mt-4 text-gray-300 dark:text-white">
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut
+						qui hic atque tenetur quis eius quos ea neque sunt,
+						accusantium soluta minus veniam tempora deserunt? Molestiae
+						eius quidem quam repellat.
+					</p>
 
-						<a
-							href="#"
-							className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-						>
-							Get Started Today
-						</a>
-					</div>
+					<a
+						href="#"
+						className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+					>
+						Get Started Today
+					</a>
 				</div>
 			</div>
-		</section>
+		</div>
 	);
 };
 
