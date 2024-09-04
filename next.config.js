e6ad297432dbc720aa8e1983e2ba8d53863e537b
dancrump1 <danielcrump@outlook.com>
2024-09-04 15:36:28 -0400

    add local assets and datefns

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
 	// output: "export", // Outputs a Single-Page Application (SPA).
 	// distDir: "./dist", // Changes the build output directory to `./dist/`.
 	// basePath: '', // Sets the base path to `/some-base-path`.
+
 	// !! WARN !!
 	// Dangerously allow production builds to successfully complete even if
 	// your project has type errors.
@@ -24,6 +25,12 @@ const nextConfig = {
 				hostname: "[website_name].com",
 				port: "",
 				pathname: "/[filesystem_name]/**",
+			},
+			{
+				protocol: "https",
+				hostname: "craft.ddev.site",
+				port: "",
+				pathname: "/public/**",
 			},
 		],
 	},
